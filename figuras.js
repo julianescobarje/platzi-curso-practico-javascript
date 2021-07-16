d7313b0bf2d1b4872381ccc275c0bd828295c357
Julian Escobar <escobarjulian347@gmail.com>
2021-07-15 20:06:30 -0500

    Se integraron las funciones en el HTML

--- figuras.js
+++ figuras.js
@@ -40,11 +40,60 @@ console.log("PI es: " + PI);
 function perimetroCirculo(radio) {
   const diametro = diametroCirculo(radio);
   return diametro * PI;
-} 
+}
 
 // Área
-function areaCirculo (radio) {
+function areaCirculo(radio) {
   return radio * radio * PI;
 }
 
 console.groupEnd();
+
+// Aquí interactuamos con el HTML
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area);
+}
+
+function calcularPerimetroTriangulo() {
+  const input = document.getElementById("InputTriangulo");
+  const value = input.value;
+
+  const perimetro = perimetroTriangulo(value);
+  alert(perimetro);
+}
+
+function calcularAreaTriangulo() {
+  const input = document.getElementById("InputTriangulo");
+  const value = input.value;
+
+  const area = areaTriangulo(value);
+  alert(area);
+}
+
+function calcularPerimetroCirculo() {
+  const input = document.getElementById("InputCirculo");
+  const value = input.value;
+
+  const perimetro = perimetroCirculo(value);
+  alert(perimetro);
+}
+
+function calcularAreaCirculo() {
+  const input = document.getElementById("InputCirculo");
+  const value = input.value;
+
+  const area = areaCirculo(value);
+  alert(area);
+}
