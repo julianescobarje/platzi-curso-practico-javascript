0fa7939aebd62f7124e07e84ba27048fe9e79e53
Julian Escobar <escobarjulian347@gmail.com>
2021-07-17 11:29:05 -0500

    Se integró la función del taller 2 en el HTML

--- taller2/descuentos.js
+++ taller2/descuentos.js
@@ -4,3 +4,19 @@ function calcularPrecioConDescuento(precio, descuento) {
 
   return precioConDescuento;
 }
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById("InputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
