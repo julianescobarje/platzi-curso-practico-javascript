170c424c06d1935a01b7827cd891d0f42b9fd069
Julian Escobar <escobarjulian347@gmail.com>
2021-07-17 11:34:09 -0500

    Se añade el bonus del reto para el taller 2

--- taller2/descuentos.js
+++ taller2/descuentos.js
@@ -1,3 +1,35 @@
+//Reto Cupones
+const coupons = [
+  {
+    name: "JuanDC_es_Batman",
+    discount: 15,
+  },
+  {
+    name: "pero_no_le_digas_a_nadie",
+    discount: 30,
+  },
+  {
+    name: "es_un_secreto",
+    discount: 25,
+  },
+];
+
+const isCouponValueValid = function (coupon) {
+  return coupon.name === couponValue;
+};
+
+const userCoupon = coupons.find(isCouponValueValid);
+
+if (!userCoupon) {
+  alert("El cupón " + userCouponValue + "no es válido");
+} else {
+  const descuento = userCoupon.discount;
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
+
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
