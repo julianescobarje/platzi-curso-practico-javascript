80ef452ac40047f1f210401763de25e506b3e435
Julian Escobar <escobarjulian347@gmail.com>
2021-07-17 19:58:56 -0500

    Se implementó la formula de la moda para el taller 3 en JS

---
+++ taller3/moda/moda.js
@@ -0,0 +1,15 @@
+const lista1 = [1, 2, 3, 1, 2, 3, 4, 2, 2, 2, 1];
+
+const lista1Count = {};
+
+lista1.map(function (elemento) {
+  if (lista1Count[elemento]) {
+    lista1Count[elemento] += 1;
+  } else {
+    lista1Count[elemento] = 1;
+  }
+});
+
+const lista1Array = Object.entries(lista1Count).sort((a, b) => a[1] - b[1]);
+
+const moda = lista1Array[lista1Array.length - 1];
