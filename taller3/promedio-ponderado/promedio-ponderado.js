a4bda163a44f5153a9110f439ea87fc5c5f06d56
Julian Escobar <escobarjulian347@gmail.com>
2021-07-18 10:34:00 -0500

    Se creó la función para hallar el promedio ponderado como bonus del taller 3

--- taller3/promedio-ponderado/promedio-ponderado.js
+++ taller3/promedio-ponderado/promedio-ponderado.js
@@ -1,35 +1,24 @@
-const notes = [
-  {
-    course: "Educación Física",
-    note: 10,
-    credit: 2,
-  },
-  {
-    course: "Programación",
-    note: 8,
-    credit: 5,
-  },
-  {
-    course: "Finanzas personales",
-    note: 7,
-    credit: 5,
-  },
-];
+function calcularPromedioPonderado(lista) {
+  const notesWithCredit = lista.map(function (noteObject) {
+    return noteObject.note * noteObject.credit;
+  });
 
-const notesWithCredit = notes.map(function (noteObject) {
-  return noteObject.note * noteObject.credit;
-});
+  const sumOfNotesWithCredit = notesWithCredit.reduce(function (
+    sum = 0,
+    newVal
+  ) {
+    return sum + newVal;
+  });
 
-const sumOfNotesWithCredit = notesWithCredit.reduce(function (sum = 0, newVal) {
-  return sum + newVal;
-});
+  const credits = lista.map(function (noteObject) {
+    return noteObject.credit;
+  });
 
-const credits = notes.map(function (noteObject) {
-  return noteObject.credit;
-});
+  const sumOfCredits = credits.reduce(function (sum = 0, newVal) {
+    return sum + newVal;
+  });
 
-const sumOfCredits = credits.reduce(function (sum = 0, newVal) {
-  return sum + newVal;
-});
+  const promedioPonderadoNotasConCreditos = sumOfNotesWithCredit / sumOfCredits;
 
-const promedioPonderadoNotasConCreditos = sumOfNotesWithCredit / sumOfCredits;
+  return promedioPonderadoNotasConCreditos;
+}
