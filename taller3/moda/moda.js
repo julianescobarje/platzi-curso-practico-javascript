0e44f5b711cbb9bf208fd26e9755bd876ee166b7
Julian Escobar <escobarjulian347@gmail.com>
2021-07-17 20:03:00 -0500

    Se creó la función para hallar la moda en el taller 3

--- taller3/moda/moda.js
+++ taller3/moda/moda.js
@@ -1,15 +1,17 @@
-const lista1 = [1, 2, 3, 1, 2, 3, 4, 2, 2, 2, 1];
+function calcularModa(lista) {
+  const listaCount = {};
 
-const lista1Count = {};
+  lista.map(function (elemento) {
+    if (listaCount[elemento]) {
+      listaCount[elemento] += 1;
+    } else {
+      listaCount[elemento] = 1;
+    }
+  });
 
-lista1.map(function (elemento) {
-  if (lista1Count[elemento]) {
-    lista1Count[elemento] += 1;
-  } else {
-    lista1Count[elemento] = 1;
-  }
-});
+  const listaArray = Object.entries(listaCount).sort((a, b) => a[1] - b[1]);
 
-const lista1Array = Object.entries(lista1Count).sort((a, b) => a[1] - b[1]);
+  const moda = listaArray[listaArray.length - 1];
 
-const moda = lista1Array[lista1Array.length - 1];
+  return moda;
+}
